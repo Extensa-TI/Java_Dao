03e8a7f56c2f492cc5434e1c89a82e50256a792c
Extensa-TI <extensati@gmail.com>
2025-06-02 12:44:26 -0300

    Implementandod findById

--- demo-jdbc-dao/src/application/Program.java
+++ demo-jdbc-dao/src/application/Program.java
@@ -1,7 +1,5 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
@@ -13,11 +11,8 @@ public class Program {
 		
 		Department obj = new Department(1, "Books");
 		
-		Seller seller = new Seller(21, "Bob", "bob@hotmail.com", new Date(), 3000.0,obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
-		System.out.println(obj);
+		Seller seller = sellerDao.findBuyId(3);
 		System.out.println(seller);
 	}
 
